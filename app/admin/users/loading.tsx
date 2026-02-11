7993339fcd064742fc1034992c5504e877e118c9
unika1 <unikaprj@gmail.com>
2026-02-11 23:45:00 +0545

    loading created

---
+++ app/admin/users/loading.tsx
@@ -0,0 +1,7 @@
+export default function AdminUsersLoading() {
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			<div className="text-zinc-600">Loading donors...</div>
+		</div>
+	);
+}
