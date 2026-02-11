3ce34fd2053c88b5febd16dc3abf2ac5da31e8a0
unika1 <unikaprj@gmail.com>
2026-02-11 23:20:30 +0545

    forget password edited

--- app/(auth)/forget-password/page.tsx
+++ app/(auth)/forget-password/page.tsx
@@ -1,4 +1,4 @@
-import ForgotPasswordForm from "../_components/forgotPasswordForm";
+import ForgotPasswordForm from "../_components/ForgotPasswordForm";
 
 export default function ForgotPasswordPage() {
   return <ForgotPasswordForm />;
